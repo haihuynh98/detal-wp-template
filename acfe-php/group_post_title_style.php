3dea63a329c1da74223f75594d86b7b811486f3a
HariHuynh <haihuynh.dev@gmail.com>
2023-08-05 10:31:12 +0700

    add acfe

---
+++ acfe-php/group_post_title_style.php
@@ -0,0 +1,93 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_post_title_style',
+	'title' => 'Title style',
+	'fields' => array(
+		array(
+			'key' => 'field_64cd1cc8e2daa',
+			'label' => 'Show image cover',
+			'name' => 'show_image_cover',
+			'aria-label' => '',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'message' => '',
+			'default_value' => 0,
+			'ui_on_text' => 'show',
+			'ui_off_text' => 'hide',
+			'ui' => 1,
+		),
+		array(
+			'key' => 'field_64cd1d1ae2dab',
+			'label' => 'Image cover',
+			'name' => 'image_cover',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 1,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_64cd1cc8e2daa',
+						'operator' => '==',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'uploader' => '',
+			'return_format' => 'url',
+			'acfe_thumbnail' => 1,
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'preview_size' => 'medium',
+			'library' => 'all',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_display_title' => '',
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1691206230,
+));
+
+endif;
