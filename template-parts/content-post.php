fedbf3f0202175e79746cae378c3c29f50c0f80c
HariHuynh <haihuynh.dev@gmail.com>
2023-08-10 23:44:33 +0700

    update stype content post

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -2,7 +2,7 @@
     <article class="pbmit-service-style-2">
         <div class="pbminfotech-post-item">
             <div class="pbmit-featured-wrapper">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/homepage-1/service/service-04.jpg" class="img-fluid" alt="">
+               <?= detal_vn_post_thumbnail()?>
             </div>
             <div class="pbminfotech-box-content">
                 <div class="pbminfotech-box-content-inner">
@@ -12,10 +12,10 @@
                         </div>
                     </div>
                     <h3 class="pbmit-service-title">
-                        <a href="#">Chỉnh nha</a>
+                        <a href="#"><?= get_the_title( )?></a>
                     </h3>
                     <div class="pbmit-service-content">
-                        <p>Chỉnh nha có thể điều chỉnh vết cắn và khôi phục cân bằng cho răng của bạn.</p>
+                        <p><?= get_the_excerpt()?></p>
                     </div>
                 </div>
             </div>
