e4a6c95430d07663d9ec8f3ad2bc993095cc8178
Hari Huynh <haihuynh.dev@gmail.com>
2023-08-15 17:57:45 +0700

    update title bar

--- template-parts/single/title-bar.php
+++ template-parts/single/title-bar.php
@@ -4,7 +4,7 @@ if (isset($args) && is_array($args)) :
 
 	<!-- Title Bar -->
 	<div class="pbmit-heading-subheading text-center" style="padding-top:200px;">
-		<h2 class="pbmit-title">The Result is here</h2>
+		<h2 class="pbmit-title"><?= $args['title']?></h2>
 	</div>
 	<!-- Title Bar End-->
 
